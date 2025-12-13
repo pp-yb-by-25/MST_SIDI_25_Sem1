962b0339ea6a143e7a85e160ab0c77abd1082398
Your Name <parcoursprofessionnel2023@gmail.com>
2025-12-13 15:11:35 +0100

    13.12.2025

--- MODULE_1_JAVA/java_master_sidi_tps/Tp2/Exercice2/Personne.java
+++
@@ -1,7 +0,0 @@
-package MODULE_1_JAVA.java_master_sidi_tps.Tp2.Exercice2;
-
-public interface Personne {
-    final int total = 500;
-    public int calculerAge();
-    public boolean esFrerede(Personne p);
-}
