00d9fee0c8f0f25734d5ee0090465806488fecaa
Your Name <parcoursprofessionnel2023@gmail.com>
2025-12-20 13:51:00 +0100

    20-12-2025

---
+++ MODULE_1_JAVA/a_Notions_java/SalaireJournalier.java
@@ -0,0 +1,17 @@
+package MODULE_1_JAVA.a_Notions_java;
+
+// 20-12-2025
+public class SalaireJournalier {
+
+    private String nomSal;
+
+
+    public static double calculSalaire(int heures, double tarif) {
+        // à compléter
+        return heures;
+    }
+
+    public static void main(String[] args) {
+        // à compléter
+    }
+}
